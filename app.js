f29bdc9bc19ddbeab2ce208b0669844e11d08edd
Leo Yockey <leoyockey@gmail.com>
2020-04-13 13:16:44 -0700

    added ability to Tweet example text

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 let textarea, emojiSpan, characterCount;
 
 const emojiString = `<span role="img" aria-label="clap">&#128079;</span>`;
+const tweetEnd = '\n\nhear how this sounds with a screenreader at https://clap.money, by @Ljyockey. #a11y'
+
+// encodes all characters encoded with encodeURIComponent, plus: ! ~ * ' ( )
+function fullyEncodeURI (value) {
+return encodeURIComponent(value)
+  .replace(/!/g, '%21')
+  .replace(/'/g, '%27')
+  .replace(/\(/g, '%28')
+  .replace(/\)/g, '%29')
+  .replace(/\*/g, '%2a')
+  .replace(/~/g, '%7e');
+}
 
 function onFormSubmit (e) {
     e.preventDefault();
@@ -8,7 +20,7 @@ function onFormSubmit (e) {
 
 function refreshCharacterCount () {
     const len = textarea.value.trim().length;
-    characterCount.innerHTML = len + '/200';
+    characterCount.innerHTML = len + '/194';
 }
 
 function refreshOtherExamples () {
@@ -21,14 +33,26 @@ function refreshOtherExamples () {
     document.getElementById('raw-text').innerHTML = replaceEmojis('');
 }
 
+function refreshTwitterData () {
+    const urlSeparator = '&text='
+
+    const v = textarea.value.trim();
+    const link = document.getElementById('tweet-link');
+
+    const baseHref = link.href.split(urlSeparator)[0];
+    const tweet = fullyEncodeURI(v + tweetEnd);
+    link.setAttribute('href', baseHref + urlSeparator + tweet);
+}
+
 function onTextareaChange ({key}) {
     const len = textarea.value.length;
-    if (key === ' ' && len < 200) {
+    if (key === ' ' && len < 194) {
         textarea.value = textarea.value += emojiSpan.innerHTML + ' ';
     }
 
     refreshCharacterCount();
     refreshOtherExamples();
+    refreshTwitterData();
 }
 
 function createEmoji () {
